70de491901a461f85bee2a45e4d5f92d88c8e0cc
EZ4ZZW <ez4zzw@163.com>
2020-09-16 16:23:40 +0800

    Test git

---
+++ Homework1/Main.java
@@ -0,0 +1,13 @@
+/**
+ * @author : EZ4ZZW
+ * @date : 2020/9/16
+ * @email: ez4zzw@163.com
+ */
+
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello GitHub!");
+        System.out.println("Test git");
+    }
+}
